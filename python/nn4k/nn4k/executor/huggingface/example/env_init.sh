f953332b9b8f16b5db43b9d4a9d3bb335641cfce
攸游 <chenbin.chen@alibaba-inc.com>
2024-01-18 20:16:13 +0800

    docs

--- python/nn4k/nn4k/executor/huggingface/example/env_init.sh
+++ python/nn4k/nn4k/executor/huggingface/example/env_init.sh
@@ -3,3 +3,4 @@
 pip install json5 # only necessary if you use json5 file as a config file
 pip install numpy==1.23.1
 pip install transformers==4.36.1
+#pip install xformers==0.0.23.post1 # only necessary if you want to accelerate loading model in memery efficient way
